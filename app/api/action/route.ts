80de97d8abaf73c05c54535ca0f4baca606aff91
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-07-13 21:19:02 +0200

    wip

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -3,13 +3,15 @@ import { appURL } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+
   const evmActionMetadata: EVMAction = {
     title: "Transfer EVM Action",
     description: "This is a sample EVM Action for transferring tokens",
     image: `${appURL()}/landing.png`,
     links: [
       {
-        targetUrl: `${appURL()}/api/tx`,
+        targetUrl: `${appURL()}/api/tx?chainId=${searchParams.get('chainId')}&tokenAddress=${searchParams.get('tokenAddress')}&toAddress=${searchParams.get('toAddress')}&amount=${searchParams.get('amount')}`,
         postUrl: `${appURL()}/tx-success`, // this will be a GET HTTP call
         label: "Tx",
         type: ActionLinkType.TX,
